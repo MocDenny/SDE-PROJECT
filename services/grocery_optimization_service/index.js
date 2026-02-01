a234d80c84b0e7ec591a909dbb0523e5d45b46de
Luca <luca.dematte2020@gmail.com>
2026-02-01 22:24:19 +0100

    Added grocery_optimization_service

---
+++ services/grocery_optimization_service/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+//const { } = require("./controllers");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const app = express();
+app.use(express.json());
+
+// Swagger configuration
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Grocery Optimization Service API",
+            version: "1.0.0",
+            description: "APIs for optimizing grocery lists based on meal plans",
+        },
+        servers: [
+            {
+                url: "http://localhost:" + process.env.GROCERY_OPT_PORT,
+                description: "Local server",
+            },
+        ],
+    },
+    apis: ["./controllers.js"], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// define route to get personalized nutrition goals (i.e calories division + diet + intolerances)
+//app.get("/goals/:email/:calories", get_info);
+
+app.listen(process.env.GROCERY_OPT_PORT, function () {
+    console.log(`Service listening on port ${process.env.GROCERY_OPT_PORT}`);
+    console.log(
+        `API documentation available at http://localhost:${process.env.GROCERY_OPT_PORT}/api-docs`,
+    );
+});
